7072233697fa3275bdd7335dc937b3d576c910d9
Maneren <49210777+Maneren@users.noreply.github.com>
2023-03-12 23:27:15 +0100

    refactor rust-analyzer settings

--- init.lua
+++ init.lua
@@ -35,6 +35,70 @@ local config = {
           offsetEncoding = "utf-8",
         },
       },
+      rust_analyzer = {
+        standalone = false,
+        settings = {
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = true,
+              -- https://github.com/rust-analyzer/rust-analyzer/issues/6835
+              disabled = { 'unresolved-macro-call' },
+              enableExperimental = true,
+            },
+            completion = {
+              autoself = { enable = true },
+              autoimport = { enable = true },
+              postfix = { enable = true },
+            },
+            imports = {
+              group = { enable = true },
+              merge = { glob = false },
+              prefix = 'self',
+              granularity = {
+                enforce = true,
+                group = 'crate',
+              },
+            },
+            cargo = {
+              loadOutDirsFromCheck = true,
+              autoreload = true,
+              runBuildScripts = true,
+              features = 'all',
+            },
+            procMacro = { enable = true },
+            lens = {
+              enable = true,
+              run = { enable = true },
+              debug = { enable = true },
+              implementations = { enable = true },
+              references = {
+                adt = { enable = true },
+                enumVariant = { enable = true },
+                method = { enable = true },
+                trait = { enable = true },
+              },
+            },
+            inlayHints = {
+              enable = true,
+              bindingModeHints = { enable = true },
+              chainingHints = { enable = true },
+              closingBraceHints = {
+                enable = true,
+                minLines = 10,
+              },
+              closureReturnTypeHints = { enable = 'always' },
+              lifetimeElisionHints = { enable = 'skip_trivial' },
+              parameterHints = { enable = false },
+              typeHints = { enable = true },
+            },
+            checkOnSave = {
+              enable = true,
+              command = 'clippy',
+              features = 'all',
+            },
+          },
+        },
+      }
     },
   },
   plugins = {
@@ -48,95 +112,6 @@ local config = {
       "simrat39/rust-tools.nvim",
       after = "mason-lspconfig.nvim", -- make sure to load after mason-lspconfig
       event = "User AstroLspSetup",
-      opts  = {
-        tools = {
-          -- rust-tools options
-          inlay_hints = {
-            auto = true,
-            only_current_line = false,
-            show_parameter_hints = true,
-          },
-          hover_actions = {
-            auto_focus = true,
-          },
-        },
-        server = {
-          standalone = false,
-          settings = {
-            ['rust-analyzer'] = {
-              diagnostics = {
-                enable = true,
-                -- https://github.com/rust-analyzer/rust-analyzer/issues/6835
-                disabled = { 'unresolved-macro-call' },
-                enableExperimental = true,
-              },
-              completion = {
-                autoself = { enable = true },
-                autoimport = { enable = true },
-                postfix = { enable = true },
-              },
-              imports = {
-                group = { enable = true },
-                merge = { glob = false },
-                prefix = 'self',
-                granularity = {
-                  enforce = true,
-                  group = 'crate',
-                },
-              },
-              cargo = {
-                loadOutDirsFromCheck = true,
-                autoreload = true,
-                runBuildScripts = true,
-                features = 'all',
-              },
-              procMacro = { enable = true },
-              lens = {
-                enable = true,
-                run = { enable = true },
-                debug = { enable = true },
-                implementations = { enable = true },
-                references = {
-                  adt = { enable = true },
-                  enumVariant = { enable = true },
-                  method = { enable = true },
-                  trait = { enable = true },
-                },
-              },
-              hover = {
-                actions = {
-                  enable = true,
-                  run = { enable = true },
-                  debug = { enable = true },
-                  gotoTypeDef = { enable = true },
-                  implementations = { enable = true },
-                  references = { enable = true },
-                },
-                links = { enable = true },
-                documentation = { enable = true },
-              },
-              inlayHints = {
-                enable = true,
-                bindingModeHints = { enable = true },
-                chainingHints = { enable = true },
-                closingBraceHints = {
-                  enable = true,
-                  minLines = 10,
-                },
-                closureReturnTypeHints = { enable = 'always' },
-                lifetimeElisionHints = { enable = 'skip_trivial' },
-                parameterHints = { enable = false },
-                typeHints = { enable = true },
-              },
-              checkOnSave = {
-                enable = true,
-                command = 'clippy',
-                features = 'all',
-              },
-            },
-          },
-        }
-      }
     },
     {
       "saecki/crates.nvim",
