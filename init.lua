07a665e4250f142ea24ea78c2975fc24072ad9ee
Maneren <49210777+Maneren@users.noreply.github.com>
2023-03-23 17:25:31 +0100

    formatting

--- init.lua
+++ init.lua
@@ -207,7 +207,7 @@ local config = {
 
         vim.ui.select = lspactions.select
         vim.ui.input = lspactions.input
-      
+
         vim.lsp.handlers["textDocument/codeAction"] = lspactions.codeaction
         vim.lsp.handlers["textDocument/references"] = lspactions.references
         vim.lsp.handlers["textDocument/definition"] = lspactions.definition
@@ -314,7 +314,7 @@ local config = {
       ["<leader>CA"] = { function() require('crates').upgrade_all_crates() end, desc = "Upgrade all crates" },
       ["<leader>Tr"] = { function() require('neotest').run.run() end, desc = "Run closest test" },
       ["<leader>Tf"] = { function() require('neotest').run.run(vim.fn.expand('%')) end, desc = "Run current file" },
-      ["<leader>Td"] = { function() require('neotest').run.run({strategy = 'dap'}) end, desc = "Debug closest test" },
+      ["<leader>Td"] = { function() require('neotest').run.run({ strategy = 'dap' }) end, desc = "Debug closest test" },
       ["<leader>Tc"] = { function() require('neotest').summary.toggle() end, desc = "Toggle summary window" },
     },
     v = {
