b1c3503f0115442c82ba7e4824a5e9855a36522c
Maneren <49210777+Maneren@users.noreply.github.com>
2023-11-06 23:45:17 +0100

    add winbar

--- plugins.lua
+++ plugins.lua
@@ -157,4 +157,47 @@ return {
       on_open = function() vim.cmd "startinsert" end,
     },
   },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require "astronvim.utils.status"
+
+      -- VSCode style winbar
+      opts.winbar = {
+        init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+        fallthrough = false,
+        {
+          condition = function() return not status.condition.is_active() end,
+          status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+          status.component.file_info {
+            file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+            file_modified = false,
+            file_read_only = false,
+            hl = status.hl.get_attributes("winbarnc", true),
+            surround = false,
+            update = "BufEnter",
+          },
+        },
+        {
+          status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+          status.component.file_info {
+            file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+            file_modified = false,
+            file_read_only = false,
+            hl = status.hl.get_attributes("winbar", true),
+            surround = false,
+            update = "BufEnter",
+          },
+          status.component.breadcrumbs {
+            icon = { hl = true },
+            hl = status.hl.get_attributes("winbar", true),
+            prefix = true,
+            padding = { left = 0 },
+          },
+        },
+      }
+
+      return opts
+    end,
+  },
 }
