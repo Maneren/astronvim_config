8d789cdd9676b047a1a84ae32be9b5f977982c6a
Maneren <49210777+Maneren@users.noreply.github.com>
2023-11-19 03:08:32 +0100

    reconfigure neovide (I want to give it another go)

--- init.lua
+++ init.lua
@@ -1,10 +1,18 @@
---              AstroNvim Configuration Table
-vim.opt.guifont = { "Iosevka", ":h14" }
+vim.opt.guifont = { "Iosevka Semibold", ":h14" }
 vim.g.neovide_fullscreen = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 vim.g.neovide_remember_window_size = true
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_animate_command_line = false
 
-local config = {
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta) vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta end
+vim.keymap.set("n", "<C-=>", function() change_scale_factor(1.1) end)
+vim.keymap.set("n", "<C-->", function() change_scale_factor(1 / 1.1) end)
+
+--    AstroNvim Configuration Table
+return {
   colorscheme = "catppuccin",
   options = {
     opt = {
@@ -13,5 +21,3 @@ local config = {
     },
   },
 }
-
-return config
