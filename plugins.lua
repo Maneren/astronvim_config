ca83dd2fc1071a2337e85092e13d9564fdecff7d
Maneren <49210777+Maneren@users.noreply.github.com>
2023-12-28 14:34:40 +0100

    replace leap with flash

--- plugins.lua
+++ plugins.lua
@@ -135,12 +135,17 @@ return {
     },
   },
   {
-    "ggandor/leap.nvim",
-    event = "BufEnter",
-    dependencies = {
-      "tpope/vim-repeat",
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-    config = function() require("leap").add_default_mappings() end,
   },
   {
     "akinsho/toggleterm.nvim",
