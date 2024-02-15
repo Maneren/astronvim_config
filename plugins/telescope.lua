969d788fe712ac58c3e2d8445408e695264a5acc
Maneren <49210777+Maneren@users.noreply.github.com>
2024-02-16 00:19:15 +0100

    add telescope cmdline plugin

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -4,12 +4,17 @@ return {
     { "nvim-lua/plenary.nvim" },
     { "HUAHUAI23/telescope-dapzzzz" },
     { "nvim-telescope/telescope-dap.nvim" },
+    { "jonarrien/telescope-cmdline.nvim" },
   },
   config = function(opts)
     local telescope = require("telescope")
     telescope.setup(opts)
     telescope.load_extension("i23")
     telescope.load_extension("dap")
+    telescope.load_extension("cmdline")
   end,
   lazy = false,
+  keys = {
+    { ":", "<cmd>Telescope cmdline<cr>", desc = "Cmdline" },
+  },
 }
