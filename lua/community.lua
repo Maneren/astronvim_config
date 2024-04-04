584fe3bf3087fa1eb3eb13dcf08e018a1b059154
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-05 00:13:33 +0200

    add sql pack

--- lua/community.lua
+++ lua/community.lua
@@ -31,6 +31,7 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
