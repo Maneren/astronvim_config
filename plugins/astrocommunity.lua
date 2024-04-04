7ee1bbb2957eccfae5a0c823cc3acca1a3c2adb4
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-04 18:26:44 +0200

    add mini.move

--- plugins/astrocommunity.lua
+++ plugins/astrocommunity.lua
@@ -16,6 +16,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.pack.bash" },
