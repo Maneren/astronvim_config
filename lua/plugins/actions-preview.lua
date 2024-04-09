31c572289cdd1bbbc4120b181cb060c563ca7682
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-09 23:32:23 +0200

    add actions preview

---
+++ lua/plugins/actions-preview.lua
@@ -0,0 +1,26 @@
+local keymaps = {
+  ["<Leader>la"] = {
+    function() require("actions-preview").code_actions() end,
+    desc = "LSP code action",
+    cond = "testDocument/codeAction",
+  },
+}
+
+---@type LazySpec
+return {
+  "aznhe21/actions-preview.nvim",
+  event = "User AstroLspSetup",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    {
+      "astrolsp",
+      ---@type AstroLSPOpts
+      opts = {
+        mappings = {
+          n = keymaps,
+          v = keymaps,
+        },
+      },
+    },
+  },
+}
