f73b01df6d4c0645a938152399bfe9c68af5726b
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-16 00:31:24 +0200

    add smartword plugin

---
+++ lua/plugins/smartword.lua
@@ -0,0 +1,11 @@
+--- @type LazySpec
+return {
+  "anuvyklack/vim-smartword",
+  event = "VeryLazy",
+  keys = {
+    { "w", "<Plug>(smartword-w)" },
+    { "b", "<Plug>(smartword-b)" },
+    { "e", "<Plug>(smartword-e)" },
+    { "ge", "<Plug>(smartword-ge)" },
+  },
+}
