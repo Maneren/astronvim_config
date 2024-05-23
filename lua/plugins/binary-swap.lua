bf090d268cd881dadd8f0fba41602a744f875c8f
Maneren <49210777+Maneren@users.noreply.github.com>
2024-05-23 14:40:36 +0200

    add binary-swap

---
+++ lua/plugins/binary-swap.lua
@@ -0,0 +1,26 @@
+--- @type LazySpec
+return {
+  "Wansmer/binary-swap.nvim",
+  event = "User AstroFile",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter" },
+    {
+      "astrocore",
+      --- @type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["gS"] = {
+              function() require("binary-swap").swap_operands() end,
+              desc = "Swap operands",
+            },
+            ["gs"] = {
+              function() require("binary-swap").swap_operands_with_operator() end,
+              desc = "Swap operands with operator",
+            },
+          },
+        },
+      },
+    },
+  },
+}
