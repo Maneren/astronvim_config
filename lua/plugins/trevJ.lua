34db71b1d122cbdaca45c20ee8794d5513802aec
Maneren <49210777+Maneren@users.noreply.github.com>
2024-05-23 14:33:33 +0200

    add trevJ

---
+++ lua/plugins/trevJ.lua
@@ -0,0 +1,22 @@
+--- @type LazySpec
+return {
+  "AckslD/nvim-trevJ.lua",
+  event = "User AstroFile",
+  opts = {},
+  dependencies = {
+    {
+      "astrocore",
+      --- @type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["gj"] = {
+              function() require("trevj").format_at_cursor() end,
+              desc = "TrevJoin",
+            },
+          },
+        },
+      },
+    },
+  },
+}
