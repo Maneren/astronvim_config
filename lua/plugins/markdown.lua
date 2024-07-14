20566188cc0d0237a7e586ac5a3f4b70d774b884
Maneren <maneren731@gmail.com>
2024-07-14 16:16:06 +0200

    migrate keybinds to astrocore table

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,16 +7,6 @@ return {
   {
     "allen-mack/nvim-table-md",
     ft = { "markdown" },
-    keys = {
-      { "<Leader>mf", function() require("tablemd").format() end, desc = "Format table" },
-      { "<Leader>mc", function() require("tablemd").insertColumn(false) end, desc = "Insert column" },
-      { "<Leader>md", function() require("tablemd").deleteColumn() end, desc = "Delete column" },
-      { "<Leader>mr", function() require("tablemd").insertRow(false) end, desc = "Insert row above" },
-      { "<Leader>mR", function() require("tablemd").insertRow(true) end, desc = "Insert row below" },
-      { "<Leader>mj", function() require("tablemd").alignColumn("left") end, desc = "Align column left" },
-      { "<Leader>mk", function() require("tablemd").alignColumn("center") end, desc = "Align column center" },
-      { "<Leader>ml", function() require("tablemd").alignColumn("right") end, desc = "Align column right" },
-    },
   },
   {
     "astrocore",
@@ -26,6 +16,15 @@ return {
           ["<Leader>m"] = { group = "󰈙 Markdown" },
           ["<Leader>mp"] = { function() vim.cmd("MarkdownPreview") end, desc = "Open preview" },
           ["<Leader>mP"] = { function() vim.cmd("MarkdownPreviewStop") end, desc = "Close preview" },
+
+          ["<Leader>mf"] = { function() require("tablemd").format() end, desc = "Format table" },
+          ["<Leader>mc"] = { function() require("tablemd").insertColumn(false) end, desc = "Insert column" },
+          ["<Leader>md"] = { function() require("tablemd").deleteColumn() end, desc = "Delete column" },
+          ["<Leader>mr"] = { function() require("tablemd").insertRow(false) end, desc = "Insert row above" },
+          ["<Leader>mR"] = { function() require("tablemd").insertRow(true) end, desc = "Insert row below" },
+          ["<Leader>mj"] = { function() require("tablemd").alignColumn("left") end, desc = "Align column left" },
+          ["<Leader>mk"] = { function() require("tablemd").alignColumn("center") end, desc = "Align column center" },
+          ["<Leader>ml"] = { function() require("tablemd").alignColumn("right") end, desc = "Align column right" },
         },
       },
       options = {
