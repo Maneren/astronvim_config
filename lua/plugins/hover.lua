253d3fbe113b05bd7e869f9c1814c4a9358a35b9
Maneren <maneren731@gmail.com>
2024-07-14 16:16:43 +0200

    configure hover.nvim

--- lua/plugins/hover.lua
+++ lua/plugins/hover.lua
@@ -5,8 +5,32 @@
 return {
   "lewis6991/hover.nvim",
   opts = {
-    init = function() require("hover.providers.lsp") end,
+    init = function()
+      require("hover.providers.lsp")
+      require("hover.providers.gh")
+      require("hover.providers.gh_user")
+      require("hover.providers.dap")
+      require("hover.providers.fold_preview")
+      require("hover.providers.diagnostic")
+      require("hover.providers.man")
+      require("hover.providers.dictionary")
+    end,
     preview_window = true,
     title = false,
+  },
+  dependencies = {
+    "astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["K"] = { function(opts) require("hover").hover(opts) end, desc = "hover.nvim" },
+          ["gK"] = {
+            function(opts) require("hover").hover_select(opts) end,
+            desc = "hover.nvim (select)",
+          },
+        },
+      },
+    },
   },
 }
