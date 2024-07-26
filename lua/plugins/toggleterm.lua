80534452d1d15c88e06d01468a9da3236152b867
Maneren <maneren731@gmail.com>
2024-07-26 20:07:43 +0200

    improve toggle term apps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,9 @@
 ---@type LazySpec
 return {
   "akinsho/toggleterm.nvim",
+  opts = {
+    start_in_insert = true,
+  },
   dependencies = {
     "astrocore",
     ---@type AstroCoreOpts
@@ -10,6 +13,18 @@ return {
           ["<Leader>tt"] = {
             function() require("toggleterm").toggle(nil, nil, "tab") end,
             desc = "ToggleTerm tab",
+          },
+          ["<Leader>tb"] = {
+            function() require("astrocore").toggle_term_cmd("btop") end,
+            desc = "ToggleTerm btop",
+          },
+          ["<Leader>tp"] = {
+            function() require("astrocore").toggle_term_cmd("ipython") end,
+            desc = "ToggleTerm ipython",
+          },
+          ["<Leader>tu"] = {
+            function() require("astrocore").toggle_term_cmd("dua i") end,
+            desc = "ToggleTerm Dua",
           },
         },
       },
