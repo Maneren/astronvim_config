ffb0a0905bf982f15d133d33c3edb010c9d2a225
Maneren <maneren731@gmail.com>
2024-08-02 20:47:01 +0200

    refactor toggleterm keybinds

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,7 @@ return {
   "akinsho/toggleterm.nvim",
   opts = {
     start_in_insert = true,
+    on_open = function() vim.cmd("startinsert!") end,
   },
   dependencies = {
     "astrocore",
@@ -11,19 +12,19 @@ return {
       mappings = {
         n = {
           ["<Leader>tt"] = {
-            function() require("toggleterm").toggle(nil, nil, "tab") end,
+            "<Cmd>ToggleTerm direction=tab<CR>",
             desc = "ToggleTerm tab",
           },
           ["<Leader>tb"] = {
-            function() require("astrocore").toggle_term_cmd("btop") end,
+            function() require("astrocore").toggle_term_cmd { cmd = "btop", direction = "float" } end,
             desc = "ToggleTerm btop",
           },
           ["<Leader>tp"] = {
-            function() require("astrocore").toggle_term_cmd("ipython") end,
+            function() require("astrocore").toggle_term_cmd { cmd = "ipython", direction = "float" } end,
             desc = "ToggleTerm ipython",
           },
           ["<Leader>tu"] = {
-            function() require("astrocore").toggle_term_cmd("dua i") end,
+            function() require("astrocore").toggle_term_cmd { cmd = "dua i", direction = "float" } end,
             desc = "ToggleTerm Dua",
           },
         },
