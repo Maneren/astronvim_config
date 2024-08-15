72c04c5e16bd0a891e08cc78694d7e4aba0901de
Maneren <maneren731@gmail.com>
2024-08-15 18:56:31 +0200

    add helpview

--- lua/community.lua
+++ lua/community.lua
@@ -23,6 +23,7 @@ return {
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
+  { import = "astrocommunity.neovim-lua-development.helpview-nvim" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cpp" },
