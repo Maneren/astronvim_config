e8c1ffa0a94e73d5577021bbce76888b05fc1e38
Maneren <maneren731@gmail.com>
2024-09-21 00:05:33 +0200

    improve markview config using astrocommunity as template

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -40,10 +40,22 @@ return {
   },
   {
     "OXY2DEV/markview.nvim",
-    ft = "markdown",
+    ft = function()
+      local plugin = require("lazy.core.config").spec.plugins["markview.nvim"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      return opts.filetypes or { "markdown", "quarto", "rmd" }
+    end,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed = require("astrocore").list_insert_unique(
+            opts.ensure_installed,
+            { "html", "markdown", "markdown_inline", "latex" }
+          )
+        end
+      end,
     },
     opts = {
       hybrid_modes = { "n" },
