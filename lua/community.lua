3c6a7729a0517245dafed4391786b7d393ecb486
Maneren <maneren731@gmail.com>
2024-09-21 00:23:15 +0200

    add neominimap and windows

--- lua/community.lua
+++ lua/community.lua
@@ -51,6 +51,8 @@ return {
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   { import = "astrocommunity.search.grug-far-nvim" },
+  { import = "astrocommunity.split-and-window.neominimap-nvim" },
+  { import = "astrocommunity.split-and-window.windows-nvim" },
   { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
   { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.utility.noice-nvim" },
