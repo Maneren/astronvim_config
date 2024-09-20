7da1981b0b3d9821db2acd3b537f71be4f6d4b40
Maneren <maneren731@gmail.com>
2024-09-20 23:19:57 +0200

    add lazydocker

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@ return {
   { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
   { import = "astrocommunity.debugging.telescope-dap-nvim" },
+  { import = "astrocommunity.docker.lazydocker" },
   { import = "astrocommunity.editing-support.dial-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.suda-vim" },
