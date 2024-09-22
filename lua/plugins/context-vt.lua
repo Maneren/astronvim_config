1a3eb90c6414c4ac99e5631a108fa0e9ac36ea89
Maneren <maneren731@gmail.com>
2024-09-22 14:27:58 +0200

    move custom plugins to community

--- lua/plugins/context-vt.lua
+++ lua/plugins/context-vt.lua
@@ -4,22 +4,7 @@
 ---@type LazySpec
 return {
   "andersevenrud/nvim_context_vt",
-  event = "User AstroFile",
   opts = {
     min_rows = 15,
-  },
-  dependencies = {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        n = {
-          ["<Leader>uv"] = {
-            function() require("nvim_context_vt").toggle_context() end,
-            desc = "Toggle virtual text context",
-          },
-        },
-      },
-    },
   },
 }
