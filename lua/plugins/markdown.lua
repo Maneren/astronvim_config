8d90f126d9f65a28b5dc7fe11b81cf44fd4e4e46
Maneren <maneren731@gmail.com>
2024-09-24 23:36:55 +0200

    fix markdown deps spec

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -46,16 +46,18 @@ return {
       return opts.filetypes or { "markdown", "quarto", "rmd" }
     end,
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        if opts.ensure_installed ~= "all" then
-          opts.ensure_installed = require("astrocore").list_insert_unique(
-            opts.ensure_installed,
-            { "html", "markdown", "markdown_inline", "latex" }
-          )
-        end
-      end,
+      { "nvim-tree/nvim-web-devicons" },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+          if opts.ensure_installed ~= "all" then
+            opts.ensure_installed = require("astrocore").list_insert_unique(
+              opts.ensure_installed,
+              { "html", "markdown", "markdown_inline", "latex" }
+            )
+          end
+        end,
+      },
     },
     opts = {
       hybrid_modes = { "n" },
