6b2eef0d7445f1b716d9a17d285376c944f42de8
Maneren <maneren731@gmail.com>
2024-10-26 17:44:04 +0200

    feat(community): add mini.ai plugin

--- lua/community.lua
+++ lua/community.lua
@@ -23,6 +23,7 @@ return {
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
