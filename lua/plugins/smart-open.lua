95fe891024efabf7e384074297c33c79a8bdbb5b
Maneren <maneren731@gmail.com>
2024-10-28 03:18:39 +0100

    feat(plugin): add smart-open

---
+++ lua/plugins/smart-open.lua
@@ -0,0 +1,19 @@
+return {
+  "danielfalk/smart-open.nvim",
+  branch = "0.2.x",
+  config = function() require("telescope").load_extension("smart_open") end,
+  dependencies = {
+    "kkharji/sqlite.lua",
+    { "nvim-telescope/telescope-fzy-native.nvim" },
+    {
+      "astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader><Leader>"] = function() require("telescope").extensions.smart_open.smart_open() end,
+          },
+        },
+      },
+    },
+  },
+}
