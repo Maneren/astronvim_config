ac6c9e5fec24cc64796b1ca20f1b543fef7a5442
Maneren <maneren731@gmail.com>
2024-11-20 01:44:38 +0100

    feat(crates): use lsp mode for completions

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -7,6 +7,19 @@ return {
   {
     "Saecki/crates.nvim",
     event = { "BufRead Cargo.toml" },
+    opts = {
+      completion = {
+        crates = {
+          enabled = true,
+        },
+      },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
   },
   {
     "astrocore",
