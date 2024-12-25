6acbdfcf4f4eb052f7a34024813936cdc1ad0e3d
Maneren <maneren731@gmail.com>
2024-12-25 23:57:22 +0100

    feat(mason): switch to tool-installer for unified tool list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,68 +1,70 @@
+--- Installer for LSPs and formatters
+--- https://github.com/williamboman/mason.nvim
+
 ---@type LazySpec
 return {
-  {
-    "williamboman/mason.nvim",
-    enabled = false,
-  },
   {
     -- TODO: switch back once https://github.com/williamboman/mason.nvim/pull/1640 is merged
     "KingMichaelPark/mason.nvim",
     name = "mason",
     opts = { pip = { use_uv = true } },
+    specs = {
+      {
+        "williamboman/mason.nvim",
+        enabled = false,
+      },
+    },
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "asm_lsp",
-        "diagnosticls",
-        "ltex",
-        "sourcery",
-        "stylelint_lsp",
-        "texlab",
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    event = "VeryLazy",
+    opts = {
+      ensure_installed = {
         "actionlint",
         "asmfmt",
+        "asm-lsp",
         "beautysh",
         "black",
+        "blackd-client",
         "cbfmt",
-        "cmakelang",
+        "codelldb",
         "cpplint",
+        "cpptools",
         "csharpier",
+        "diagnostic-languageserver",
         "fixjson",
         "jsonlint",
+        "ltex-ls",
+        "markdown-oxide",
         "markdownlint",
+        "neocmakelsp",
         "prettierd",
         "robotframework-lsp",
-        "rustywind",
         "shellcheck",
         "shellharden",
         "sonarlint-language-server",
+        "sourcery",
         "sqlfmt",
+        "stylelint-lsp",
+        "texlab",
+        "twiggy-language-server",
         "yamlfmt",
         "yamllint",
-      })
-      opts.handlers.prettierd = function(source_name, methods)
-        local null_ls = require("null-ls")
-        for _, method in ipairs(methods) do
-          null_ls.register(null_ls.builtins[method][source_name].with { filetypes = { "php", "twig" } })
-        end
-      end
-    end,
+      },
+    },
   },
   {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "codelldb",
-        "cpptools",
-      })
-    end,
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      handlers = {
+        -- enable prettier for php and twig
+        prettierd = function(source_name, methods)
+          local null_ls = require("null-ls")
+          for _, method in ipairs(methods) do
+            null_ls.register(null_ls.builtins[method][source_name].with { filetypes = { "php", "twig" } })
+          end
+        end,
+      },
+    },
   },
 }
