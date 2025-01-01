020b30e584c47edaa544aedb09939471b9a24252
Maneren <maneren731@gmail.com>
2025-01-01 03:21:04 +0100

    feat(none-ls): use fork with fixed unicode handling
    ref: https://github.com/nvimtools/none-ls.nvim/issues/226

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,12 @@
 ---@type LazySpec
 return {
   { "max397574/better-escape.nvim", enabled = false },
+  {
+    -- TODO: use fork until https://github.com/nvimtools/none-ls.nvim/issues/226 is
+    -- resolved
+    "nvimtools/none-ls.nvim",
+    url = "https://github.com/Maneren/none-ls.nvim",
+    branch = "unicode-length",
+    commit = false,
+  },
 }
