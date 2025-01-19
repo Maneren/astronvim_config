9dbba505efaaaebceba7eb15816c47dc8a504efd
Maneren <maneren731@gmail.com>
2025-01-19 14:48:12 +0100

    feat(mason): enable prettier for markdown files

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -50,11 +50,11 @@ return {
     "jay-babu/mason-null-ls.nvim",
     opts = {
       handlers = {
-        -- enable prettier for php and twig
+        -- enable prettier for more languages
         prettierd = function(source_name, methods)
           local null_ls = require("null-ls")
           for _, method in ipairs(methods) do
-            null_ls.register(null_ls.builtins[method][source_name].with { filetypes = { "php", "twig" } })
+            null_ls.register(null_ls.builtins[method][source_name].with { filetypes = { "php", "twig", "markdown" } })
           end
         end,
       },
