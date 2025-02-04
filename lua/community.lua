ab8c0b92dd4d2811ed20187b46768fe717c014d0
Maneren <maneren731@gmail.com>
2025-02-04 23:38:01 +0100

    feat(community): lazydev was upstreamed directly to AstroNvim

--- lua/community.lua
+++ lua/community.lua
@@ -26,7 +26,6 @@ return {
   { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
   { import = "astrocommunity.neovim-lua-development.helpview-nvim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.cmake" },
