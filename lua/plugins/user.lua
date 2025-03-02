b7a73c8cc3e4e3f362380223597c26a8a7cda167
Maneren <maneren731@gmail.com>
2025-03-02 22:09:49 +0100

    feat(none-ls): use main repo again
    https://github.com/nvimtools/none-ls.nvim/issues/226 was fixed

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,14 +1,6 @@
 ---@type LazySpec
 return {
   { "max397574/better-escape.nvim", enabled = false },
-  {
-    -- TODO: use fork until https://github.com/nvimtools/none-ls.nvim/issues/226 is
-    -- resolved
-    "nvimtools/none-ls.nvim",
-    url = "https://github.com/Maneren/none-ls.nvim",
-    branch = "unicode-length",
-    commit = false,
-  },
   { "AstroNvim/astrocore", version = false, branch = "v2" },
   { "AstroNvim/astrolsp", version = false, branch = "v3" },
   { "AstroNvim/astroui", version = false, branch = "v3" },
