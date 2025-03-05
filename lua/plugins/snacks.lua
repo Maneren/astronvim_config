1683ca19530b4f35dd583ea52ef6c6b447123180
Maneren <maneren731@gmail.com>
2025-03-05 23:14:33 +0100

    feat(snacks): enable debugging helpers

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -140,4 +140,14 @@ return {
       },
     },
   },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        _G.dd = function(...) require("snacks").debug.inspect(...) end
+        _G.bt = function() require("snacks").debug.backtrace() end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+      end,
+    })
+  end,
 }
