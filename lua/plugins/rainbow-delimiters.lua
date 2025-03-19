1ba53ada1135c81462f614838a7437fa537e8eec
Maneren <maneren731@gmail.com>
2025-03-19 19:47:23 +0100

    feat(rainbow-delimiters): highlight latex blocks

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,14 @@
+--- Rainbow delimiters
+--- https://github.com/HiPhish/rainbow-delimiters.nvim
+--- extends astrocommunity
+
+---@type LazySpec
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  opts = {
+    query = {
+      [""] = "rainbow-delimiters",
+      latex = "rainbow-blocks",
+    },
+  },
+}
