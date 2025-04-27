bd4ebfee1d491c97751f33dd40763992cc4178fc
Maneren <maneren731@gmail.com>
2025-04-27 21:20:59 +0200

    feat(plugins): add sibling swap

--- lua/plugins/binary-swap.lua
+++ lua/plugins/binary-swap.lua
@@ -1,26 +1,68 @@
---- Swaps binary operands
+--- Swap binary operands and TS siblings
 --- https://github.com/Maneren/binary-swap.nvim
+--- https://github.com/Wansmer/sibling-swap.nvim
 
 ---@type LazySpec
 return {
-  "Maneren/binary-swap.nvim",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter" },
-  },
-  specs = {
-    {
-      "astrocore",
-      ---@type AstroCoreOpts
-      opts = {
-        mappings = {
-          n = {
-            ["gS"] = {
-              function() require("binary-swap").swap_operands() end,
-              desc = "Swap operands",
+  {
+    "Maneren/binary-swap.nvim",
+    event = "User AstroFile",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    specs = {
+      {
+        "astrocore",
+        ---@type AstroCoreOpts
+        opts = {
+          mappings = {
+            n = {
+              ["gS"] = {
+                function() require("binary-swap").swap_operands() end,
+                desc = "Swap operands",
+              },
+              ["gs"] = {
+                function() require("binary-swap").swap_operands_with_operator() end,
+                desc = "Swap operands with operator",
+              },
             },
-            ["gs"] = {
-              function() require("binary-swap").swap_operands_with_operator() end,
-              desc = "Swap operands with operator",
+          },
+        },
+      },
+    },
+  },
+  {
+    "Wansmer/sibling-swap.nvim",
+    event = "User AstroFile",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    opts = {
+      use_default_keymaps = false,
+    },
+    specs = {
+      {
+        "astrocore",
+        ---@type AstroCoreOpts
+        opts = {
+          mappings = {
+            n = {
+              [">s"] = {
+                function() require("sibling-swap").swap_with_left() end,
+                desc = "Swap next sibling",
+              },
+              ["<s"] = {
+                function() require("sibling-swap").swap_with_right() end,
+                desc = "Swap previous sibling",
+              },
+              [">S"] = {
+                function() require("sibling-swap").swap_with_left_with_opp() end,
+                desc = "Swap next sibling (opposite)",
+              },
+              ["<S"] = {
+                function() require("sibling-swap").swap_with_right_with_opp() end,
+                desc = "Swap previous sibling (opposite)",
+              },
             },
           },
         },
