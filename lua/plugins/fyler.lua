e76c6baccbbef41907c632ab225132e681764e4c
Maneren <maneren731@gmail.com>
2025-06-30 00:32:00 +0200

    feat(plugins): add fyler

---
+++ lua/plugins/fyler.lua
@@ -0,0 +1,10 @@
+--- File manager
+--- https://github.com/A7Lavinraj/fyler.nvim
+
+--- @type LazySpec
+return {
+  "A7Lavinraj/fyler.nvim",
+  event = "VeryLazy",
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+}
