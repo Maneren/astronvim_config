b4740b92a96627eb42aa6cbb7d3dafbba846c184
Maneren <maneren731@gmail.com>
2025-10-02 14:26:24 +0200

    feat(fyler): add toggle mapping

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -5,6 +5,17 @@
 return {
   "A7Lavinraj/fyler.nvim",
   event = "VeryLazy",
+  cmd = { "Fyler" },
   dependencies = { "echasnovski/mini.icons" },
+  version = "*",
   opts = {},
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        maps.n["<Leader>E"] = { function() require("fyler").toggle { kind = "split_left_most" } end, desc = "Fyler" }
+      end,
+    },
+  },
 }
