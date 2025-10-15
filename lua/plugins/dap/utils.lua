d48133bcf05d332c4b26a99133ccc690617b763d
Maneren <maneren731@gmail.com>
2025-10-16 00:33:37 +0200

    feat(dap): update C config
    Copied from C++ config

--- lua/plugins/dap/utils.lua
+++ lua/plugins/dap/utils.lua
@@ -237,17 +237,24 @@ return {
       run {
         {
           name = "Launch",
-          type = "lldb",
+          type = "codelldb",
           request = "launch",
           program = require("dap.utils").pick_file,
           cwd = "${workspaceFolder}",
         },
         {
           name = "Launch with args",
-          type = "lldb",
+          type = "codelldb",
           request = "launch",
           program = require("dap.utils").pick_file,
           args = parse_args,
+          cwd = "${workspaceFolder}",
+        },
+        {
+          name = "Attach",
+          type = "codelldb",
+          request = "attach",
+          pid = require("dap.utils").pick_process,
           cwd = "${workspaceFolder}",
         },
       }
