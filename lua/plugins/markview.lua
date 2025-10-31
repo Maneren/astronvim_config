e4c6a90ff14a650512c92f35a9823332d8052582
Maneren <maneren731@gmail.com>
2025-10-31 12:04:56 +0100

    feat(markview): add gradient and sqrt

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -28,6 +28,7 @@ return {
       Var = "Var",
       bit = "bit",
       jinak = "jinak",
+      grad = "grad",
     })
     local presets = require("markview.presets")
     return {
@@ -102,7 +103,47 @@ return {
 
                     virt_text_pos = "inline",
                     virt_text = {
-                      { "|", "@punctuation.bracket" },
+                      { "|", "@punctuation.bracket.latex" },
+                    },
+
+                    hl_mode = "combine",
+                  }
+                end,
+              },
+            },
+          },
+          sqrt = {
+            condition = function(item) return #item.args == 1 end,
+            on_command = {
+              conceal = "",
+            },
+            on_args = {
+              {
+                on_before = function(item)
+                  return {
+                    end_col = item.range[2] + 1,
+                    conceal = "",
+
+                    virt_text_pos = "inline",
+                    virt_text = {
+                      { "√", "@keyword.function" },
+                      { "(", "@punctuation.bracket.latex" },
+                    },
+
+                    hl_mode = "combine",
+                  }
+                end,
+
+                after_offset = function(range) return { range[1], range[2], range[3], range[4] - 1 } end,
+
+                on_after = function(item)
+                  return {
+                    end_col = item.range[4],
+                    conceal = "",
+
+                    virt_text_pos = "inline",
+                    virt_text = {
+                      { ")", "@punctuation.bracket" },
                     },
 
                     hl_mode = "combine",
