ba2018399eca28326fa01ecca3abf0d03b2acb84
Maneren <maneren731@gmail.com>
2025-11-27 00:53:37 +0100

    feat(hydra): add windows hydra

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -2,12 +2,24 @@
 --- https://github.com/nvimtools/hydra.nvim
 --- extends astrocommunity
 
--- currently only used for side scroll
+local window_hint = [[
+ ^^^^^^^^^^^^     Move      ^^    Size   ^^   ^^     Split
+ ^^^^^^^^^^^^-------------  ^^-----------^^   ^^---------------
+ ^ ^ _k_ ^ ^  ^ ^ ^ _K_ ^ ^    ^   _+_   ^      _s_: horizontally
+ _h_ ^ ^ _l_  ^ _H_ ^ ^ _L_    _<_     _>_      _v_: vertically
+ ^ ^ _j_ ^ ^  ^ ^ ^ _J_ ^ ^    ^   _-_   ^      _q_: close
+ focus^^^^^^  ^window^^^^  ^^^_=_: equalize ^   _z_: max height
+ ^ ^ ^ ^ ^ ^  ^ ^ ^ ^ ^ ^ ^  ^^ ^           ^   _x_: max width
+ ^ _b_: choose buffer ^ ^^^ ^ ^ ^ ^ ^ ^ ^^ ^^   _o_: remain only
+]]
 
 ---@type LazySpec
 return {
   "nvimtools/hydra.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
   opts = {
     ["Side scroll"] = {
       mode = "n",
@@ -17,6 +29,50 @@ return {
         { "l", "5zl", { desc = "→ one column" } },
         { "H", "zH", { desc = "← half screen" } },
         { "L", "zL", { desc = "→ half screen" } },
+      },
+    },
+    ["Windows"] = {
+      hint = window_hint,
+      config = {
+        invoke_on_body = true,
+      },
+      mode = "n",
+      body = "<C-w>",
+      heads = {
+        { "h", "<C-w>h" },
+        { "j", "<C-w>j" },
+        { "k", "<C-w>k" },
+        { "l", "<C-w>l" },
+
+        { "H", "<C-w>H" },
+        { "J", "<C-w>J" },
+        { "K", "<C-w>K" },
+        { "L", "<C-w>L" },
+
+        { "<", "<C-w><" },
+        { "-", "<C-w>-" },
+        { "+", "<C-w>+" },
+        { ">", "<C-w>>" },
+        { "=", "<C-w>=", { desc = "equalize" } },
+
+        { "s", "<C-w>s" },
+        { "v", "<C-w>v" },
+
+        { "w", "<C-w>w", { exit = true, desc = false } },
+        { "<C-w>", "<C-w>w", { exit = true, desc = false } },
+        { "z", "<c-w>_", { desc = "maximize height" } },
+        { "x", "<c-w>|", { desc = "maximize width" } },
+        { "o", "<C-w>o", { exit = true, desc = "remain only" } },
+
+        {
+          "b",
+          function() require("snacks").picker.buffers() end,
+          { exit = true, desc = "choose buffer" },
+        },
+
+        { "q", ":q<CR>", { desc = "close window" } },
+
+        { "<Esc>", nil, { exit = true, desc = false } },
       },
     },
   },
